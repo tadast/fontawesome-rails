35f4b57675a90071738aca8c42b90ad8299ae13c
Michael Coyne <mikeycgto@gmail.com>
2015-10-05 18:07:27 -0400

    Make sure all fonts are precompiled

--- lib/fontawesome/rails/railtie.rb
+++ lib/fontawesome/rails/railtie.rb
@@ -2,7 +2,9 @@ require 'pry'
 module Fontawesome
   module Rails
     class Railtie < ::Rails::Railtie
-      config.assets.precompile += %w( fontawesome-webfont.eot fontawesome-webfont.woff fontawesome-webfont.ttf fontawesome-webfont.svg )
+      config.assets.precompile += %w(
+        FontAwesome.otf  fontawesome-webfont.eot  fontawesome-webfont.svg  fontawesome-webfont.ttf  fontawesome-webfont.woff  fontawesome-webfont.woff2
+      )
     end
   end
 end
