e88606b8407387fe4395bcc8f6e016b1da2b4451
Michael Coyne <mikeycgto@gmail.com>
2015-10-10 10:41:55 -0400

    More updates to properly handle asset paths

--- lib/fontawesome/rails/railtie.rb
+++ lib/fontawesome/rails/railtie.rb
@@ -3,7 +3,7 @@ module Fontawesome
   module Rails
     class Railtie < ::Rails::Railtie
       config.assets.precompile += %w(
-        FontAwesome.otf  fontawesome-webfont.eot  fontawesome-webfont.svg  fontawesome-webfont.ttf  fontawesome-webfont.woff  fontawesome-webfont.woff2
+        FontAwesome.otf fontawesome-webfont.eot fontawesome-webfont.svg fontawesome-webfont.ttf fontawesome-webfont.woff fontawesome-webfont.woff2
       )
     end
   end
