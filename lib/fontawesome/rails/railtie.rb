c29f2a675564ac3c649ddc2822d5a74fd8486f0e
Jurgen Jocubeit <jurgenjocubeit@gmail.com>
2016-02-23 18:15:48 +1000

    Removed "require 'pry'", it has no dependency.

--- lib/fontawesome/rails/railtie.rb
+++ lib/fontawesome/rails/railtie.rb
@@ -1,9 +1,7 @@
-require 'pry'
 module Fontawesome
   module Rails
     class Railtie < ::Rails::Railtie
       config.assets.paths << File.join(File.dirname(__FILE__), '..', '..', '..', 'vendor', 'assets', 'fonts').to_s
-
       config.assets.precompile += %w( .eot .woff .ttf)
     end
   end
